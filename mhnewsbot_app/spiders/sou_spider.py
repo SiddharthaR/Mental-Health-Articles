41225af2288676df2d78cd8d2cfcfd0d0ec2b559
Sid Rajgarhia <ssr3010@gmail.com>
2017-08-18 18:35:31 +0530

    Added multiple attributes to spider

--- mhnewsbot_app/spiders/sou_spider.py
+++ mhnewsbot_app/spiders/sou_spider.py
@@ -11,5 +11,9 @@ class SOUSpider(Spider):
 	def parse(self, response):
 		for article in response.xpath('//ul[@class="newsfeed-article-list"]'):
 			return {
-	        'title': article.xpath('.//li[contains(@class, "newsfeed-article")]/div[@class="headline-wrapper"]/a[@class="headline-link"]/h3[@class="headline"]/text()').extract()
+	        'title': article.xpath('.//li[contains(@class, "newsfeed-article")]/div[@class="headline-wrapper"]/a[@class="headline-link"]/h3[@class="headline"]/text()').extract(),
+	        'teaser': article.xpath('.//li[contains(@class, "newsfeed-article")]/p[@class = "teaser"]/text()').extract(),
+	        'link': article.xpath('.//li[contains(@class, "newsfeed-article")]/a/@href').extract(),
+	        'date': article.xpath('.//li[contains(@class, "newsfeed-article")]/div[@class="headline-wrapper"]/div[@class="headline-above"]/time/text()').extract(),
+	        'author': article.xpath('.//li[contains(@class, "newsfeed-article")]/span[@class="by-authors"]/span/span[@class="author"]/text()').extract()
 	        }
