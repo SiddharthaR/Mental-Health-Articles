424574b28788fb03a7b13c9ca3dd368edafbb283
Sid Rajgarhia <ssr3010@gmail.com>
2017-09-08 22:10:02 +0800

    Changed output format to fit database correctly

--- mhnewsbot_app/spiders/sou_spider.py
+++ mhnewsbot_app/spiders/sou_spider.py
@@ -6,7 +6,7 @@ import string
 mh_search_terms = ["DEPRESS", "MENTAL HEALTH", "EMOTIONAL HEALTH", "MENTAL DISORDER", "DIGITAL MEDICINE", "ANXI", "PSYCH", "THERAPY", "THERAPIST"]
 tbl = string.maketrans('-', ' ') #To protect against cases where the article has hyphens or other special characters
 
-articles = {'title': [], 'teaser': [], 'link': [], 'date': [], 'author': [], 'source': []}
+#articles = {'title': [], 'teaser': [], 'link': [], 'date': [], 'author': [], 'source': []}
 
 # Create list of urls for scrapy to crawl. Hardcoded to follow Science of us format. 
 def url_lister():
@@ -28,10 +28,11 @@ class SOUSpider(Spider):
 	        for i in title:
 	        	for search_term in mh_search_terms:
 		        	if search_term in i.upper().strip():
-						articles['title'].append(article.xpath('.//li[contains(@class, "newsfeed-article")]/div[@class="headline-wrapper"]/a[@class="headline-link"]/h3[@class="headline"]/text()').extract()[title.index(i)])
-						articles['teaser'].append(article.xpath('.//li[contains(@class, "newsfeed-article")]/p[@class = "teaser"]/text()').extract()[title.index(i)])
-						articles['link'].append(article.xpath('.//li[contains(@class, "newsfeed-article")]/a[@class = "read-more"]/@href').extract()[title.index(i)])
-						articles['date'].append(article.xpath('.//li[contains(@class, "newsfeed-article")]/div[@class="headline-wrapper"]/div[@class="headline-above"]/time/text()').extract()[title.index(i)])
-						articles['author'].append(article.xpath('.//li[contains(@class, "newsfeed-article")]/span[@class="by-authors"]/span/span[@class="author"]/text()').extract()[title.index(i)])
-						articles['source'].append('Science Of Us')
-		return articles
+						article_item = {}
+						article_item['title'] = article.xpath('.//li[contains(@class, "newsfeed-article")]/div[@class="headline-wrapper"]/a[@class="headline-link"]/h3[@class="headline"]/text()').extract()[title.index(i)]
+						article_item['teaser'] = article.xpath('.//li[contains(@class, "newsfeed-article")]/p[@class = "teaser"]/text()').extract()[title.index(i)]
+						article_item['link'] = article.xpath('.//li[contains(@class, "newsfeed-article")]/a[@class = "read-more"]/@href').extract()[title.index(i)]
+						article_item['date'] = article.xpath('.//li[contains(@class, "newsfeed-article")]/div[@class="headline-wrapper"]/div[@class="headline-above"]/time/text()').extract()[title.index(i)]
+						article_item['author'] = article.xpath('.//li[contains(@class, "newsfeed-article")]/span[@class="by-authors"]/span/span[@class="author"]/text()').extract()[title.index(i)]
+						article_item['source'] ='Science Of Us'
+						yield article_item
